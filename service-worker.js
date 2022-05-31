b4e57528755eebedf64e6f012b0c33bca44cb1a4
padster06 <paddy2006@hotmail.co.uk>
2022-05-31 23:03:50 +0100

    changed service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,15 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
-);
+const KEY = "key";
 
-workbox.routing.registerRoute(
-  ({ request }) => request.destination === "image",
-  new workbox.strategies.NetworkFirst()
-);
+self.addEventListener("install", (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data.type === "CACHE_URLS") {
+    event.waitUntil(
+      caches.open(KEY).then((cache) => {
+        return cache.addAll(event.data.payload);
+      })
+    );
+  }
+});
